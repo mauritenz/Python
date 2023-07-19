a3adeadec2aff49e77d7b61c4a80b0a8867afc9f
mauritenz <74872740+mauritenz@users.noreply.github.com>
2023-07-19 15:17:22 -0300

    Ejercicios del curso de python

---
+++ index.py
@@ -0,0 +1,27 @@
+
+#edad = int(input("Que valor tiene x ? :"))  
+
+#if edad >= 18 :
+#    print("El usuario es mayor de edad")
+#else:
+#    print("No es mayor de edad")
+
+#Escribir un programa que pida al usuario dos números y devuelva su división. 
+#Si el usuario no introduce numero debe devolver un aviso de error y si el divisor es cero también. 
+
+#valor1 = int(input("Ingrese el primer valor :"))
+#valor2 = int(input("Ingrese el segundo valor :"))
+
+#if valor2 == 0:
+#     print("Error, no es posible dividir por cero. ")
+#else:
+#     print(valor1/valor2)
+
+#Escribir un programa que pida un numero y que identifique si es par o impar.
+
+nro = int(input("ingrese valor para saber si es par :"))
+
+if nro % 2 == 0:
+     print("El numero es par")
+else:
+     print("El numero es impar")
